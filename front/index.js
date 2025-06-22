7d3f8f73c2616ab350fbe65bea91f1839fd1fa84
lolosola <dsola@pioix.edu.ar>
2025-06-22 17:22:19 -0300

    inicio

---
+++ front/index.js
@@ -0,0 +1,15 @@
+//hola no se que hcaer xd lol
+
+function register(){ //igna tenes que hacer el appi de agregar un usuario a la bd
+
+    let user = ui.getUser()
+    let fullName = ui.getFullName()
+    let password = ui.getPassword()
+
+    fetch //aca va la funcion post app que tenes que hacer igna pasado por parametro el user, fullName (nombre y apellido juntos), password, y el id que es el dni
+
+} 
+
+function logIn(){
+
+}
