07dca2c31e21d7412a802f69c627c2ea8273fbd0
lolosola <dsola@pioix.edu.ar>
2025-06-22 18:09:48 -0300

    conectado el css

--- front/index.js
+++ front/index.js
@@ -1,6 +1,6 @@
 //hola no se que hcaer xd lol
 
-function register(){ // @igna tenes que hacer el appi de agregar un usuario a la bd
+function register() { // @igna tenes que hacer el appi de agregar un usuario a la bd
 
     let user = ui.getUser()
     let fullName = ui.getFullName()
@@ -9,18 +9,18 @@ function register(){ // @igna tenes que hacer el appi de agregar un usuario a la
 
     fetch //aca va la funcion post app que tenes que hacer @igna pasado por parametro el user, fullName (nombre y apellido juntos), password, y el id que es el dni
 
-} 
-function fetchCheckUser(id,user,password){
+}
+function fetchCheckUser(id, user, password) {
     fetch //aca va la funcion post app que tenes que hacer @igna pasado por parametro el user, password, y el id que es el dni para verificar que exista el usuario y que sea correcto
 }
-function logIn(){ //necesito un appi que verifique que exista este usuario con este password @igna
+function logIn() { //necesito un appi que verifique que exista este usuario con este password @igna
     let id = ui.getId()
     let user = ui.getUser()
     let password = ui.getPassword()
-    if(fetchCheckUser(id,user,password)){
-        
+    if (fetchCheckUser(id, user, password)) {
+
     }
-    
-   
-    
+
+
+
 }
